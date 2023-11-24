127a61743a0bd01c0b5a0b3a24b9c116a29b1744
EndCar808 <enda.carroll@ucdconnect.ie>
2023-11-24 17:42:06 +0000

    Updated some gatsby-config stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+// Set max listeners to suppress MaxListenersExceededWarning
+process.setMaxListeners(30);
+
 const config = require('./src/config');
 
 module.exports = {
@@ -9,6 +12,7 @@ module.exports = {
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@EndaCar',
   },
+
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
@@ -20,8 +24,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Enda Carroll',
+        short_name: 'Enda Carroll',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
