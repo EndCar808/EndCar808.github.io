06d517d437f4cd2154a14f82f2c50fc4c2dfa787
EndCar808 <enda.carroll@ucdconnect.ie>
2023-11-25 14:46:07 +0000

    Editing the hero.js page

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,18 +60,25 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Enda Carrol.</h2>;
+  const three = (
+    <h3 className="big-heading">I like to make sense of the world via the lense of mathematics.</h3>
+  );
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
+        I am a recently graduated applied mathematician with a PhD from{' '}
+        <a href="https://www.ucd.ie/mathstat/" target="_blank" rel="noreferrer">
+          UCD
+        </a>
+        .
+        {/* I’m a software engineer specializing in building (and occasionally designing) exceptional
         digital experiences. Currently, I’m focused on building accessible, human-centered products
         at{' '}
         <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
           Upstatement
         </a>
-        .
+        .*/}
       </p>
     </>
   );
